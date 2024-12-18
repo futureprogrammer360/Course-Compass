8ce5d31a15fafa57d35c660f7ebcfd55475a3ac2
Ian Le <ianlllym@gmail.com>
2024-12-18 00:43:02 -0800

    Integrate api.get_course into api.get_courses with autocompletion and result limiting

--- api/api.py
+++ api/api.py
@@ -4,9 +4,10 @@ API providing access to the database that stores course data
 """
 
 import os
+from typing import Annotated
 
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Path, Query
 from fastapi.middleware.cors import CORSMiddleware
 import motor.motor_asyncio
 
@@ -27,24 +28,43 @@ db = client["db"]
 
 
 @app.get("/courses/{university_id}")
-async def get_courses(university_id: str):
-    """Get courses associated with a particular university"""
-    documents = await db["courses"].find({
-        "university_id": university_id
-    }).to_list()
+async def get_courses(
+    university_id: Annotated[str, Path(title="University ID")],
+    limit: Annotated[int, Query(gt=0)] = None,
+    query: str = None
+):
+    """Get courses associated with a particular university
 
+    If a query is given, get list of courses with matching course numbers (up to limit, if given)
+    If no query is given, get list of all courses associated with given university (up to limit, if given)
+
+    Args:
+        university_id: University ID
+        limit: Maximum number of courses returned
+        query: Query to be matched against course numbers
+
+    Returns:
+        A list of dicts containing data on courses
+
+    Raises:
+        HTTPException: No courses found with the given arguments
+    """
+    if query:
+        pipeline = [
+            {"$search": {"index": "courses-autocomplete", "autocomplete": {"query": query, "path": "number"}}},
+            {"$match": {"university_id": university_id}}
+        ]
+        if limit:
+            pipeline.append({"$limit": limit})
+        documents = db["courses"].aggregate(pipeline)
+    else:
+        documents = db["courses"].find({
+            "university_id": university_id
+        })
+        if limit:
+            documents = documents.limit(limit)
+
+    documents = await documents.to_list()
     if len(documents) == 0:
-        return {"code": 404}
-    return {"code": 200, "courses": documents}
-
-
-@app.get("/course/{university_id}/{course_id}")
-async def get_course(university_id: str, course_id: str):
-    """Get a course by university ID and course ID"""
-    document = await db["courses"].find_one({
-        "_id": f"{university_id}-{course_id}"
-    })
-
-    if document is None:
-        return {"code": 404}
-    return {"code": 200, "course": document}
+        raise HTTPException(status_code=404, detail="No courses found")
+    return documents
